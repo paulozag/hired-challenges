f09a2d2701e2d9beddbebd8b4f626c8b409388b3
paulozag <ozag@comcast.net>
2016-06-14 17:53:31 -0700

    refactors challenge 4 a little

--- challenge_4/scratch.rb
+++ challenge_4/scratch.rb
@@ -27,10 +27,10 @@ class Deviation_Finder
 
   def solve
     populate_high_array
-    # populate_low_array
     differential_array = []
-    high_ranges.each_with_index {|v,i|
-      differential_array << v-@array[i]}
+    high_ranges.each_with_index do |v,i|
+      differential_array << v-@array[i]
+    end
     differential_array.max
   end
 
@@ -41,10 +41,15 @@ class Deviation_Finder
   def high_slice i_start, i_end
     max_index = @array[i_start..i_end].each_with_index.max[1]
     max_value = @array[max_index + i_start]
+
     push_values max_index + i_start, max_value, @high_ranges
-    # binding.pry
-    (high_slice max_index + i_start + 1,i_end) unless (max_index + @d + i_start) >= i_end
-    (high_slice i_start, max_index + i_start - 1) unless (max_index - @d + i_start) <= i_start
+
+    unless (max_index + @d + i_start) >= i_end
+      (high_slice max_index + i_start + 1,i_end)
+    end
+    unless (max_index - @d + i_start) <= i_start
+      (high_slice i_start, max_index + i_start - 1)
+    end
   end
 
   def low_slice i_start, i_end
