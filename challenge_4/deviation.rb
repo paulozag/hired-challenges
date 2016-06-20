2409cc6ed411b2a0c934648a6383486e5ff073cc
paulozag <ozag@comcast.net>
2016-06-20 15:55:35 -0700

    finishes refactor of challenge_4

--- challenge_4/deviation.rb
+++ challenge_4/deviation.rb
@@ -1,20 +1,11 @@
-require 'pry'
+# require 'pry'
 
-array = [6,9,4,7,4,1]
-d_len = 3
 
-#  to begin, write an algorithm to map the indices for the ranges of +/- d by highest value.
-
-# test data
-test_array = []
-100000.times do
-  test_array << rand(0..100000000000)
-end
-test_d = rand(2...100000)
 
 class Deviation_Finder
 
-  attr_accessor :array, :high_ranges, :low_ranges
+  attr_accessor :array, :high_ranges
+
   def initialize arr, d
     @array    = arr
     @length    = arr.length
@@ -27,9 +18,12 @@ class Deviation_Finder
 
   def solve
     populate_high_array
+    # above method populates :high_ranges - this is an array whose values represent the highest value up to d elements away
     differential_array = []
     high_ranges.each_with_index do |v,i|
       differential_array << v-@array[i]
+      # this differential array now represents the difference between the highest value this index could be to the current value.  the max value in this array
+      # will represent the greatest deviation in a d length list of elements of the initial array
     end
     differential_array.max
   end
@@ -41,22 +35,18 @@ class Deviation_Finder
   def high_slice i_start, i_end
     max_index = @array[i_start..i_end].each_with_index.max[1]
     max_value = @array[max_index + i_start]
-
     push_values max_index + i_start, max_value, @high_ranges
 
     unless (max_index + @d + i_start) >= i_end
       (high_slice max_index + i_start + 1,i_end)
     end
-    unless (max_index - @d + i_start) <= i_start
+
+    unless (max_index - @d ) <= 0
       (high_slice i_start, max_index + i_start - 1)
     end
   end
 
-  def low_slice i_start, i_end
-    min_index = @array[i_start..i_end].each_with_index.min[1]
-    min_value = @array[min_index]
-    push_values min_index, min_value, @low_ranges
-  end
+
 
   def push_values index, value, array
     start = [0, (index - @d)].max
@@ -66,13 +56,12 @@ class Deviation_Finder
 
 end
 
-data = [1,5,3,9,12,3,2,0,6]
+test_array = []
+100000.times do
+  test_array << rand(0..100000000000)
+end
+test_d = rand(2...100000)
 
 a = Deviation_Finder.new(test_array, test_d)
-
-# p a.array
-# a.solve
-# p a.array
-# p a.high_ranges
 
 p a.solve
---
+++ challenge_4/test_data_challenge_4.rb
@@ -0,0 +1,3 @@
+#  to begin, write an algorithm to map the indices for the ranges of +/- d by highest value.
+
+# test data
